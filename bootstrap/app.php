5146e7191086447288156126908eb6b0364dec23
MNovalA <novalagustian43@gmail.com>
2026-06-01 13:43:55 +0700

    fix: trust proxies for HTTPS on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
         'role' => RoleMiddleware::class,
+        
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
