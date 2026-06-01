0802ccf5667f9f676b4a1766a002c1645043ab74
MNovalA <novalagustian43@gmail.com>
2026-06-01 12:26:45 +0700

    rev index/api

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
-require __DIR__ . '/.../public/index.php';
+$publicPath = __DIR__ . '/../../public';
+
+// Serve static files
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
+    return false;
+}
+
+// Bootstrap Laravel
+define('LARAVEL_START', microtime(true));
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
